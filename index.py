e94976042678a4c4c19517bde9b5bef7e37d7aef
Abdillahabualrob <abed2004.aburub@gmail.com>
2025-06-17 19:19:52 +0300

    كود الخوارزميات

---
+++ index.py
@@ -0,0 +1,42 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = merge_sort(arr[:mid])
+        right = merge_sort(arr[mid:])
+
+        merged = []
+        i = j = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                merged.append(left[i])
+                i += 1
+            else:
+                merged.append(right[j])
+                j += 1
+
+        merged.extend(left[i:])
+        merged.extend(right[j:])
+        return merged
+    else:
+        return arr
+
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [x for x in arr[1:] if x < pivot]
+        equal = [x for x in arr if x == pivot]
+        greater = [x for x in arr[1:] if x > pivot]
+        return quick_sort(less) + equal + quick_sort(greater)
